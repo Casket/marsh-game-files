2bcfd0bf9af72ccc854faf492c7d20a8fc2e098e
glenngs <glenngs@rose-hulman.edu>
2013-04-26 16:00:21 -0400

    guard ftw

--- Marsh/Marsh/Town_Guard.h
+++ Marsh/Marsh/Town_Guard.h
@@ -19,39 +19,38 @@ class Town_Guard:public Combat{
 
 	std::vector<std::pair<int, int>>* waypoints;
 	
-	bool on_patrol, up_or_down, initial_detection, in_combat, paused, x_or_y, follow_detour;
-	bool patrolling_forward;
+	bool on_patrol, in_combat, x_or_y;
+	bool patrolling_forward, detour;
 	std::vector<std::pair<int,Direction>>* path, ret_path;
+	Direction cur_dir;
 	iDrawable* target;
-	int patrol_frame_count, flip_frames, reset_node;
+	int flip_frames;
 	int patrol_node;
 	bool movement_blocked;
 	iDrawable* blocking_entity;
-	int frame_counter_for_direction_change;
+	std::pair<int, int> target_area;
+	std::pair<std::pair<int, int>, std::pair<int, int>> detour_pair;
+	std::list<iDrawable*> TargsToChoose; 
 
 	public:
 		Town_Guard(int x, int y, int vel, int vel_d, Sprite* img, std::vector<std::pair<int,int>>* waypoints);
 		~Town_Guard(void);
 		virtual void update(void);
-		void patrol(void);
 		bool move(Direction new_dir);
 	
 	protected:
 		virtual void check_collisions(void);
-		std::pair<int, int> detour_obstruction(void);
+		std::pair<std::pair<int, int>, std::pair<int, int>> detour_obstruction(void);
 		bool get_current_facing_flag(void);
 
 
 	private:
 		Direction switch_dir(Direction old_dir);
-		void detect_enemies(iDrawable* to_check);
+		bool detect_enemies(iDrawable* to_check);
 		bool get_visible_to_guard(iDrawable*);
-		void forward(std::pair<int, Direction> cur_node);
-		void reverse(std::pair<int, Direction> cur_node);
 		void other_check_collisions(void);
 		void find_path(void);
 		bool move_towards(std::pair<int, int>);
-		void reset(void);
 		void increment_patrol(void);
 };
 
