9395dd23f894982918f260ad282c2585309c0009
dylansturg <sturgedl@rose-hulman.edu>
2013-05-07 11:58:10 -0400

    updating death beam

--- Marsh/Marsh/Combat.cpp
+++ Marsh/Marsh/Combat.cpp
@@ -18,8 +18,8 @@ Combat::Combat(int x, int y, int vel, int vel_d, Sprite* img)
 	this->attack_loadout[0] = this->attack_loadout[0]->clone(0, 0, W);
 	this->attack_loadout[0]->set_my_caster(this);
 
-	Attack_Sprite* death = new Attack_Sprite("Resources//Attack Sprites//Death_Beam_edited.bmp", N, 5, 1, 4, 4, 75, 59);
-	death->set_state_frame_counts(1, 1, 2);
+	Attack_Sprite* death = new Attack_Sprite("Resources//Attack Sprites//Death_Beam_end.bmp", N, 5, 1, 3, 3, 80, 60);
+	death->set_state_frame_counts(0, 1, 2);
 	AttackStatistics stats;
 	stats.base_damage = 0;
 	stats.charge_time = 100;
@@ -35,8 +35,8 @@ Combat::Combat(int x, int y, int vel, int vel_d, Sprite* img)
 
 	this->attack_loadout[1] = death_beam;
 	
-	Attack_Sprite* shooter_img = new Attack_Sprite("Resources//Attack Sprites//Death_Beam_edited.bmp", N, 5, 1, 4, 4, 75, 59);
-	shooter_img->set_state_frame_counts(1, 1, 0);
+	Attack_Sprite* shooter_img = new Attack_Sprite("Resources//Attack Sprites//Death_Beam_charge.bmp", N, 5, 1, 1, 1, 50, 19);
+	shooter_img->set_state_frame_counts(0, 1, 0);
 
 	stats.base_damage = 0;
 	stats.charge_time = 100;
@@ -44,9 +44,9 @@ Combat::Combat(int x, int y, int vel, int vel_d, Sprite* img)
 	stats.penetration = 15;
 	stats.range = 250;
 	stats.tree_depth = 2;
-	MultiSpawnAttack* beamer = new MultiSpawnAttack(800, 800, 2, 10, death, stats, death_beam, 30);
-	beamer->set_position_adjustment(40, 20);
-	beamer->set_boundary_value(75, 20, 5, 20);
+	MultiSpawnAttack* beamer = new MultiSpawnAttack(800, 800, 2, 10, shooter_img, stats, death_beam, 30);
+	beamer->set_position_adjustment(20, 0);
+	beamer->set_boundary_value(50, 19, 0, 0);
 	beamer->set_my_caster(this);
 	this->attack_loadout[2] = beamer;
 
