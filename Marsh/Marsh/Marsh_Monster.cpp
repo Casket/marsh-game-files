c85d6e1f3a4c96a7f8ab77a0fd6b3879c93002ad
glenngs <glenngs@rose-hulman.edu>
2013-05-09 15:06:05 -0400

    yo yo yo again

---
+++ Marsh/Marsh/Marsh_Monster.cpp
@@ -0,0 +1,16 @@
+#include "Main.h"
+
+
+using namespace std;
+
+
+Marsh_Monster::Marsh_Monster(int x, int y, int vel, int vel_d, Sprite* img, std::vector<std::pair<int,int>>* waypoints)
+:Mob(x, y,vel, vel_d, img, waypoints){
+
+	
+
+}
+Marsh_Monster::~Marsh_Monster(void){
+
+
+}
